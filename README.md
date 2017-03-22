3fbb48462c7c3df33e15dd2e67de5a4f95c129e8
sfilatov96 <stepa-filatov@yandex.ru>
2017-03-22 12:30:38 +0300

    версия с файлами

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ instagram_json_parser
 Утилита позволяет вытащить количество записей с определенным тегом
 </p>
 <h3>Команда:</h3> 
-<code>python main.py <Название тега> </code>
+<code>python main.py  </code>
+
+В файл tags.txt пишем теги, а в файле answer.txt ждём ответы.
 
 
 (python2.7)
--- main.py
+++ main.py
@@ -2,27 +2,49 @@
 from bs4 import BeautifulSoup
 import urllib
 import json
+from operator import itemgetter
 from sys import argv
 
+
+
+
+def parser(hashtag):
+    page = urllib.urlopen(("https://www.instagram.com/explore/tags/%s/" % (hashtag)))
+    soup = BeautifulSoup(page,"lxml")
+    scripts = soup.find_all("script")
+    preparing_json = None
+    for s in scripts:
+        if s.string:
+            if "window._sharedData" in s.string:
+                preparing_json = s.string
+
+
+    preparing_json = preparing_json[:-1]
+    preparing_json = preparing_json.replace("window._sharedData = ","")
+    preparing_json = json.loads(preparing_json)
+    tag = preparing_json["entry_data"]["TagPage"]
+    count = tag[0]["tag"]["media"]
+    count = count["count"]
+    return count,hashtag
+
+
 try:
-    hashtag = argv[1]
+    filename = argv[1]
+    file = open("%s" % filename,"r")
 except:
-    hashtag = "sport"
+    file = open("tags.txt","r")
+record = open("answer.txt","w")
 
-page = urllib.urlopen(("https://www.instagram.com/explore/tags/%s/" % (hashtag)))
-soup = BeautifulSoup(page,"lxml")
-scripts = soup.find_all("script")
-preparing_json = None
-for s in scripts:
-    if s.string:
-        if "window._sharedData" in s.string:
-            preparing_json = s.string
+tags = file.read()
+tags = tags.split("\n")
+tags = tags[:-1]
 
+tags = [parser(i) for i in tags ]
 
-preparing_json = preparing_json[:-1]
-preparing_json = preparing_json.replace("window._sharedData = ","")
-preparing_json = json.loads(preparing_json)
-tag = preparing_json["entry_data"]["TagPage"]
-count = tag[0]["tag"]["media"]
-count = count["count"]
-print str(count) + "  Публикаций"
+tags = sorted(tags,key=lambda x: x[0], reverse=True)
+
+for i,j in tags:
+    print str(i) +' '+ j
+    record.write(str(i) +' '+ j+'\n')
+
+record.close()
